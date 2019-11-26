19d629c89bfb45b3380211c894a63cc4d38f57e3
henry <tarou.88@gmail.com>
2019-11-26 10:53:32 +0900

    :art: map launches to launch screen
    map graphql query to launchtiles

--- src/screens/Launches.tsx
+++ src/screens/Launches.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import styled from 'styled-components/native';
 import gql from 'graphql-tag';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
-import Button from '../components/Button';
 import { LaunchTile } from '../components/LaunchTile';
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
 
 const GET_LAUNCHES = gql`
   query launchList($after: String) {
@@ -12,30 +26,28 @@ const GET_LAUNCHES = gql`
       cursor
       hasMore
       launches {
-        id
-        isBooked
-        rocket {
-          id
-          name
-        }
-        mission {
-          name
-          missionPatch
-        }
+        ...LaunchTile
       }
     }
   }
+  ${LAUNCH_TILE_DATA}
 `;
 
 export const Launches: React.FC = () => {
-  const { error } = useQuery(GET_LAUNCHES);
-  if (error) return <p>ERROR</p>;
-
+  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  if (loading) return <Text>Loading</Text>;
+  if (error) return <Text>error</Text>;
   return (
     <SafeArea>
       <Container>
-        <LaunchTile />
-        <Button text="load more" />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
+        >
+          {data.launches.launches.map(launch => (
+            <LaunchTile key={launch.id} mission={launch.mission} />
+          ))}
+        </ScrollView>
       </Container>
     </SafeArea>
   );
@@ -49,4 +61,10 @@ const Container = styled.View`
   height: 100%;
   background-color: ${({ theme }): string => theme.colors.background};
   align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.Text`
+  font-size: 18px;
+  color: ${({ theme }): string => theme.colors.text};
 `;
